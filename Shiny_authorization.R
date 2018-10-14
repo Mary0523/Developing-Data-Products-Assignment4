176cee95788e66f9d9c900b99a704dbae02ab387
Mary0523 <mayinjiao@gmail.com>
2018-10-14 15:33:40 -0500

    remove

--- Shiny_authorization.R
+++
@@ -1,5 +0,0 @@
-rsconnect::setAccountInfo(name='mary0523',
-                          token='BB10BD17F161C775F8C813568FC866F5',
-                          secret='kXidf+vLQm7+CWXmcpD8hxuzj686U/rsdHkO///+')
-library(rsconnect)
-rsconnect::deployApp('C:/Users/ZTQ/Desktop/finalproject/myApp')
